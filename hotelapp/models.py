b4a3d1c2231175db410e755075b2fc8f13256a48
shublynx <shubham683075@gmail.com>
2023-06-16 15:49:38 +0530

    Adding on github

--- hotelapp/models.py
+++ hotelapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Amenities(models.Model):
+    amenity = models.CharField(max_length=100)
+
+
+class Hotel(models.Model):
+    hotel_name = models.CharField(max_length=100)
+    hotel_price = models.FloatField()
+    amenities = models.ManyToManyField(Amenities)
+    banner_image = models.ImageField(upload_to="Images/", default="Images/Noimg.jpg")
+
+
+class HotelImages(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    hotel_pics = models.ImageField(upload_to="Images/", default="Images/Noimg.jpg")
